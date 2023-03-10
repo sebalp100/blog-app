b253fde8e2093cbf426bd10d67880c92fd4400c1
sebalp100 <sebalp100@gmail.com>
2023-03-10 13:14:19 -0300

    Add data and post/user methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :posts
+
+  def comments_counter
+    posts.update(comments_counter: user.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  after_save :posts_counter
+
+  def posts_counter
+    user.update(post_counter: user.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
