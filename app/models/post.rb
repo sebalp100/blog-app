f2bd76442cf626c5fc88bcae4ba189c91487b4a4
sebalp100 <sebalp100@gmail.com>
2023-03-10 22:43:30 -0300

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
 
   after_save :posts_counter
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def posts_counter
     author.update(post_counter: author.posts.count)
   end
