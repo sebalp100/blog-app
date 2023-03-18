2b7c7c2c12795c07e3084ab6adb64c0d05992111
sebalp100 <sebalp100@gmail.com>
2023-03-18 16:48:27 -0300

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,9 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = @post.author
-    if params[:post_number]
-      redirect_to user_post_path(@user, @post)
-    end
+    redirect_to user_post_path(@user, @post) if params[:post_number]
     @post_number = @user.posts.find_index(@post) + 1
-    rescue ActiveRecord::RecordNotFound
-      render file: "#{Rails.root}/public/404.html", status: :not_found
+  rescue ActiveRecord::RecordNotFound
+    render file: "#{Rails.root}/public/404.html", status: :not_found
   end
 end
