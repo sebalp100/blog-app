61d750a64102d1ad74787faee4bc06faa79eb75c
sebalp100 <sebalp100@gmail.com>
2023-03-18 16:27:01 -0300

    Add posts data and final style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = @post.author
+    if params[:post_number]
+      redirect_to user_post_path(@user, @post)
+    end
+    @post_number = @user.posts.find_index(@post) + 1
     rescue ActiveRecord::RecordNotFound
       render file: "#{Rails.root}/public/404.html", status: :not_found
   end
