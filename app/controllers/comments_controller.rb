9862124c5bc79f61c1819f44533a3fa6e9a8c537
Amen TETTEH <tkamen@outlook.com>
2023-03-24 16:37:05 +0000

    Update comments_controller.rb file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,36 +4,33 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
-
   def create
     @user = User.find(params[:user_id])
-
     if @user.id != current_user.id
-    flash[:alert] = "You cannot create comments for another user."
-    redirect_to root_path and return
+      flash[:alert] = "You cannot create comments for another user."
+      redirect_to root_path and return
     end
-
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.author = current_user
-
     if @comment.save
       redirect_to user_posts_path, notice: 'Comment was successfully created.'
     else
       render :new
     end
-
-    def destroy
-      @comment.destroy
-      @comment.update(comments_counter: @post.comments_counter - 1)
-      respond_to do |format|
+  end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    @comment.destroy
+    @post.update(comments_counter: @post.comments_counter - 1)
+  
+    respond_to do |format|
       format.html { redirect_to root_path, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
-
   private
-
   def comment_params
     params.require(:comment).permit(:text)
   end
