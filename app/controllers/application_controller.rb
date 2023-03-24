410a23b476a244311bfc5e54f6fd50cf5c27fd61
sebalp100 <sebalp100@gmail.com>
2023-03-24 14:04:28 -0300

    Fix linter error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
   before_action :authenticate_user!
 
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
