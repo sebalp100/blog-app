3d2820fe5574264ef9ccf591029541986a64eaa2
sebalp100 <sebalp100@gmail.com>
2023-03-24 16:49:21 -0300

    Add posts and comments endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,19 @@ Rails.application.routes.draw do
       resources :comments, only: [:create, :new, :destroy, :index]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index]
+      end
+      resources :posts, only: [] do
+        resources :comments, only: [:index]
+      end
+    end
+  end
+
+  # http://[::1]:3000/api/v1/users/1/posts 
+  # http://[::1]:3000/api/v1/posts/21/comments
   root "users#index"
 end
